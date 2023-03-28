54aaede4b928b9ee8139ad1b8266fc5b995727de
TheHiberNate <nathan@gawargy.com>
2023-03-28 12:24:01 -0400

    Update Chopstick.java

--- JavaCode/Chopstick.java
+++ JavaCode/Chopstick.java
@@ -1,19 +1,29 @@
 public class Chopstick {
 	private int ID;
+	private boolean chopstickFree;
 // hint: use a local variable to indicate whether the chopstick is free 
 //                        (lying on the table), e.g.  private boolean free;
 
 	Chopstick(int ID) {
 		  this.ID = ID;
-	
+		  chopstickFree = true;
 	}
 	
-	synchronized void take() {
-	
+	synchronized void take()  {
+		if (chopstickFree) {
+			chopstickFree = false;
+		} else {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	synchronized void release() {
-	
+		notify();
+		chopstickFree = true;
 	}
 	    
 	public int getID() {
